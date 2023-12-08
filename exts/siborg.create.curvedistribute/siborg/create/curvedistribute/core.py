13ee2532f330a613da6067291228efbba1de7234
cadop <cadop@users.noreply.github.com>
2023-12-08 18:18:49 -0500

    connect to ui

--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
@@ -3,39 +3,49 @@ import numpy as np
 from scipy.interpolate import BSpline
 import omni.usd
 
-stage = omni.usd.get_context().get_stage()
-print(stage)
 
-# Create an xform which should hold all references in this sample
+def interpcurve(stage, curve_path, num_points):
+
+    # Get the curve prim and points that define it
+    curveprim = stage.GetPrimAtPath(curve_path)
+    points = curveprim.GetAttribute('points').Get()
+    points = np.array(points)
+    # print(f'Points: {points}')
+
+    # Create a BSpline object
+    k = 3 # degree of the spline
+    t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
+    t = np.append(np.zeros(k), t)
+    t = np.append(t, np.ones(k))
+    spl = BSpline(t, points, k)
+
+    # Interpolate points
+    tnew = np.linspace(0, 1, num_points)
+    interpolated_points = spl(tnew)
+
+    return interpolated_points
+
+def copy_to_points(stage, interpolated_points, ref_prim, path_to):
+
+    for i in range(len(interpolated_points)):
+        primpath_to = f"{path_to}_{i}"
+        omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
+        new_prim = stage.GetPrimAtPath(primpath_to)
+        target_pos = Gf.Vec3d(tuple(interpolated_points[i]))
+        new_prim.GetAttribute('xformOp:translate').Set(target_pos)
+
+
+stage = omni.usd.get_context().get_stage()
+curve_path = '/World/BasisCurves'
 ref_prim = '/World/Cube'
+path_to = f'{ref_prim}Copy'
 num_points = 15
 
-prim = stage.GetPrimAtPath('/World/BasisCurves')
-points = prim.GetAttribute('points').Get()
-points = np.array(points)
-print(f'Points: {points}')
 
-# Extract x, y, and z coordinates
-x, y, z = points[:, 0], points[:, 1], points[:, 2]
-
-# Create a BSpline object
-k = 3 # degree of the spline
-t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
-t = np.append(np.zeros(k), t)
-t = np.append(t, np.ones(k))
-spl = BSpline(t, points, k)
-
-# Interpolate points
-tnew = np.linspace(0, 1, num_points)
-interpolated_points = spl(tnew)
+interpolated_points = interpcurve(stage, curve_path, num_points)
+copy_to_points(stage, interpolated_points, ref_prim, path_to)
 
 
-for i in range(len(interpolated_points)):
-    path_to = Sdf.Path(f"/World/intern_target_{i}")
-    omni.usd.duplicate_prim(stage, ref_prim, path_to)
-    new_prim = stage.GetPrimAtPath(path_to)
-    target_pos = Gf.Vec3d(tuple(interpolated_points[i]))
-    new_prim.GetAttribute('xformOp:translate').Set(target_pos)
 
 # # Add an internal references 
 # for i in range(num_points):
--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
@@ -1,11 +1,43 @@
 import omni.ext
 import omni.ui as ui
 
+from pxr import Usd, UsdGeom, Gf, Sdf
+import numpy as np
+from scipy.interpolate import BSpline
+import omni.usd
 
-# Functions and vars are available to other extension as usual in python: `example.python_ext.some_public_function(x)`
-def some_public_function(x: int):
-    print("[siborg.create.curvedistribute] some_public_function was called with x: ", x)
-    return x ** x
+
+def interpcurve(stage, curve_path, num_points):
+
+    # Get the curve prim and points that define it
+    curveprim = stage.GetPrimAtPath(curve_path)
+    points = curveprim.GetAttribute('points').Get()
+    points = np.array(points)
+    # print(f'Points: {points}')
+
+    # Create a BSpline object
+    k = 3 # degree of the spline
+    t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
+    t = np.append(np.zeros(k), t)
+    t = np.append(t, np.ones(k))
+    spl = BSpline(t, points, k)
+
+    # Interpolate points
+    tnew = np.linspace(0, 1, num_points)
+    interpolated_points = spl(tnew)
+
+    return interpolated_points
+
+def copy_to_points(stage, interpolated_points, ref_prim, path_to):
+
+    for i in range(len(interpolated_points)):
+        primpath_to = f"{path_to}_{i}"
+        omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
+        new_prim = stage.GetPrimAtPath(primpath_to)
+        target_pos = Gf.Vec3d(tuple(interpolated_points[i]))
+        new_prim.GetAttribute('xformOp:translate').Set(target_pos)
+
+
 
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
@@ -19,25 +51,25 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
 
         self._count = 0
 
-        self._window = ui.Window("My Window", width=300, height=300)
+        self._window = ui.Window("Distribute along curve", width=300, height=300)
         with self._window.frame:
             with ui.VStack():
                 label = ui.Label("")
 
+                def duplicate():
+                    stage = omni.usd.get_context().get_stage()
+                    curve_path = '/World/BasisCurves'
+                    ref_prim = '/World/Cube'
+                    path_to = f'{ref_prim}Copy'
+                    num_points = self._count
 
-                def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
+                    interpolated_points = interpcurve(stage, curve_path, num_points)
+                    copy_to_points(stage, interpolated_points, ref_prim, path_to)
 
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
-
-                with ui.HStack():
-                    ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
+                with ui.VStack():
+                    x = ui.IntField(height=5) 
+                    x.model.add_value_changed_fn(lambda m, self=self: setattr(self, '_count', m.get_value_as_int()))
+                    ui.Button("Distribute", clicked_fn=duplicate)
 
     def on_shutdown(self):
         print("[siborg.create.curvedistribute] siborg create curvedistribute shutdown")
