1ffbcf5fe574efd80f69cf9d369cddd2534839a5
cadop <cadop@users.noreply.github.com>
2023-12-12 18:00:03 -0500

    Revert "update point copy to take list of prims and alternate distributions"
    This reverts commit 0e3357df392fa7af5e8b35c6bce08a79620b09a8.

--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
@@ -28,33 +28,15 @@ def interpcurve(stage, curve_path, num_points):
 
     return interpolated_points
 
-def copy_to_points(stage, interp_points, ref_prims, path_to, rand_order=False, use_orient=False, follow_curve=False):
-    '''
-    
-    path_to: str, prefix to the prim path. automatically appends Copy
-    TODO: rand_order =True, use randomness to determine which prim to place
-    TODO: use_orient=True, rotate object to fact direction 
-    TODO: follow_curve=True, set rotation axis to match curve (different than orientation when curve is 3d)
-    '''
+def copy_to_points(stage, interpolated_points, ref_prim, path_to):
 
-    # Get index of prims that are not None and keep track 
-    prim_set = [p for p in ref_prims if p != None]
-    print(prim_set) 
-    num_prims = len(prim_set)
-    cur_idx = 0
-
-    for i in range(len(interp_points)):
-        ref_prim = prim_set[cur_idx]
-        primpath_to = f"{ref_prim}Copy_{i}"
-        
+    for i in range(len(interpolated_points)):
+        primpath_to = f"{path_to}_{i}"
         omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
         new_prim = stage.GetPrimAtPath(primpath_to)
-        target_pos = Gf.Vec3d(tuple(interp_points[i]))
+        target_pos = Gf.Vec3d(tuple(interpolated_points[i]))
         new_prim.GetAttribute('xformOp:translate').Set(target_pos)
 
-        # Go to the next prim, or reset to 0
-        if cur_idx < num_prims-1: cur_idx +=1
-        else: cur_idx = 0
 
 
 
@@ -76,19 +58,13 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
 
                 def duplicate():
                     stage = omni.usd.get_context().get_stage()
-
                     curve_path = '/World/BasisCurves'
-
-                    ## All of these should work (assuming the named prim is there)
-                    ref_prims = ['/World/Cube']
-                    # ref_prims = ['/World/Cube', None]
-                    # ref_prims = ['/World/Cube', '/World/Cone']
-
-                    path_to = f'/Copy'
+                    ref_prim = '/World/Cube'
+                    path_to = f'{ref_prim}Copy'
                     num_points = self._count
 
                     interpolated_points = interpcurve(stage, curve_path, num_points)
-                    copy_to_points(stage, interpolated_points, ref_prims, path_to)
+                    copy_to_points(stage, interpolated_points, ref_prim, path_to)
 
                 with ui.VStack():
                     x = ui.IntField(height=5) 
