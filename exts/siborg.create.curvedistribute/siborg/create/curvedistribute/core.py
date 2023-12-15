0655c97ca4367da39c664ff0e576a16db1ce2328
cadop <cadop@users.noreply.github.com>
2023-12-15 11:53:49 -0500

    move curve tools out of main ext file

--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
@@ -4,55 +4,112 @@ from scipy.interpolate import BSpline
 import omni.usd
 
 
-def interpcurve(stage, curve_path, num_points):
+class CurveManager():
+    def __init__(self):
+        pass
 
-    # Get the curve prim and points that define it
-    curveprim = stage.GetPrimAtPath(curve_path)
-    points = curveprim.GetAttribute('points').Get()
-    points = np.array(points)
-    # print(f'Points: {points}')
+    @classmethod
+    def interpcurve(cls, stage, curve_path, num_points):
 
-    # Create a BSpline object
-    k = 3 # degree of the spline
-    t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
-    t = np.append(np.zeros(k), t)
-    t = np.append(t, np.ones(k))
-    spl = BSpline(t, points, k)
+        # Get the curve prim and points that define it
+        curveprim = stage.GetPrimAtPath(curve_path)
+        points = curveprim.GetAttribute('points').Get()
+        points = np.array(points)
+        # print(f'Points: {points}')
 
-    # Interpolate points
-    tnew = np.linspace(0, 1, num_points)
-    interpolated_points = spl(tnew)
+        # Create a BSpline object
+        k = 3 # degree of the spline
+        t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
+        t = np.append(np.zeros(k), t)
+        t = np.append(t, np.ones(k))
+        spl = BSpline(t, points, k)
 
-    return interpolated_points
+        # Interpolate points
+        tnew = np.linspace(0, 1, num_points)
+        interpolated_points = spl(tnew)
 
-def copy_to_points(stage, interpolated_points, ref_prim, path_to):
+        return interpolated_points
 
-    for i in range(len(interpolated_points)):
-        primpath_to = f"{path_to}_{i}"
-        omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
-        new_prim = stage.GetPrimAtPath(primpath_to)
-        target_pos = Gf.Vec3d(tuple(interpolated_points[i]))
-        new_prim.GetAttribute('xformOp:translate').Set(target_pos)
+    @classmethod
+    def copy_to_points(cls, stage, target_points, ref_prims, path_to, 
+                        rand_order=False, use_orient=False, follow_curve=False):
+        '''
+        
+        path_to: str, prefix to the prim path. automatically appends Copy
+        TODO: rand_order =True, use randomness to determine which prim to place
+        TODO: use_orient=True, rotate object to fact direction 
+        TODO: follow_curve=True, set rotation axis to match curve (different than orientation when curve is 3d)
+        '''
+
+        # Get index of prims that are not None and keep track 
+        prim_set = [p for p in ref_prims if p != None]
+        print(prim_set) 
+        num_prims = len(prim_set)
+        cur_idx = 0
+
+        for i in range(len(target_points)):
+            ref_prim = prim_set[cur_idx]
+            primpath_to = f"{ref_prim}Copy_{i}"
+            
+            omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
+            new_prim = stage.GetPrimAtPath(primpath_to)
+            target_pos = Gf.Vec3d(tuple(target_points[i]))
+            new_prim.GetAttribute('xformOp:translate').Set(target_pos)
+
+            # Go to the next prim, or reset to 0
+            if cur_idx < num_prims-1: cur_idx +=1
+            else: cur_idx = 0
 
 
-stage = omni.usd.get_context().get_stage()
-curve_path = '/World/BasisCurves'
-ref_prim = '/World/Cube'
-path_to = f'{ref_prim}Copy'
-num_points = 15
+    # stage = omni.usd.get_context().get_stage()
+    # curve_path = '/World/BasisCurves'
+    # ref_prim = '/World/Cube'
+    # path_to = f'{ref_prim}Copy'
+    # num_points = 15
 
 
-interpolated_points = interpcurve(stage, curve_path, num_points)
-copy_to_points(stage, interpolated_points, ref_prim, path_to)
+    # interpolated_points = interpcurve(stage, curve_path, num_points)
+    # copy_to_points(stage, interpolated_points, ref_prim, path_to)
+
+    # # Add an internal references 
+    # for i in range(num_points):
+    #     intern_target_path = Sdf.Path(f"/World/intern_target_{i}")
+    #     target_prim = UsdGeom.Xform.Define(stage, intern_target_path).GetPrim()
+    #     references  = ref_prim.GetReferences()
+    #     references.AddInternalReference(intern_target_path)
 
 
+class GeomCreator():
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def duplicate(cls, _count, _source_curve_model, _source_prim_model):
 
-# # Add an internal references 
-# for i in range(num_points):
-#     intern_target_path = Sdf.Path(f"/World/intern_target_{i}")
-#     target_prim = UsdGeom.Xform.Define(stage, intern_target_path).GetPrim()
-#     references  = ref_prim.GetReferences()
-#     references.AddInternalReference(intern_target_path)
+        stage = omni.usd.get_context().get_stage()
+        curve_path = _source_curve_model.as_string
+        ref_prims = [_source_prim_model.as_string]
 
+        ## All of these should work (assuming the named prim is there)
+        # ref_prims = ['/World/Cube']
+        # ref_prims = ['/World/Cube', None]
+        # ref_prims = ['/World/Cube', '/World/Cone']
 
+        path_to = f'/Copy'
 
+        num_points = _count
+        # Default to 3x the number of points to distribute? Actually might be handled already by interp
+        num_samples = _count
+
+        # TODO: make this setting for the resolution to sample the curve defined by user
+        interpolated_points = CurveManager.interpcurve(stage, curve_path, num_samples)
+        indices = np.linspace(0, len(interpolated_points) - 1, num_points, dtype=int)
+        target_points = interpolated_points[indices]
+        CurveManager.copy_to_points(stage, target_points, ref_prims, path_to)
+        
+                
+        #     # TODO maybe?: make this setting for the resolution to sample the curve defined by user
+        #     interpolated_points = CurveManager.interpcurve(stage, curve_path, num_samples)
+        #     indices = np.linspace(0, len(interpolated_points) - 1, num_points, dtype=int)
+        #     target_points = interpolated_points[indices]
+        #     CurveManager.copy_to_points(stage, target_points, ref_prims, path_to)
--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
@@ -7,57 +7,7 @@ from scipy.interpolate import BSpline
 import omni.usd
 from .utils import get_selection
 
-
-def interpcurve(stage, curve_path, num_points):
-
-    # Get the curve prim and points that define it
-    curveprim = stage.GetPrimAtPath(curve_path)
-    points = curveprim.GetAttribute('points').Get()
-    points = np.array(points)
-    # print(f'Points: {points}')
-
-    # Create a BSpline object
-    k = 3 # degree of the spline
-    t = np.linspace(0, 1, len(points) - k + 1, endpoint=True)
-    t = np.append(np.zeros(k), t)
-    t = np.append(t, np.ones(k))
-    spl = BSpline(t, points, k)
-
-    # Interpolate points
-    tnew = np.linspace(0, 1, num_points)
-    interpolated_points = spl(tnew)
-
-    return interpolated_points
-
-def copy_to_points(stage, target_points, ref_prims, path_to, rand_order=False, use_orient=False, follow_curve=False):
-    '''
-    
-    path_to: str, prefix to the prim path. automatically appends Copy
-    TODO: rand_order =True, use randomness to determine which prim to place
-    TODO: use_orient=True, rotate object to fact direction 
-    TODO: follow_curve=True, set rotation axis to match curve (different than orientation when curve is 3d)
-    '''
-
-    # Get index of prims that are not None and keep track 
-    prim_set = [p for p in ref_prims if p != None]
-    print(prim_set) 
-    num_prims = len(prim_set)
-    cur_idx = 0
-
-    for i in range(len(target_points)):
-        ref_prim = prim_set[cur_idx]
-        primpath_to = f"{ref_prim}Copy_{i}"
-        
-        omni.usd.duplicate_prim(stage, ref_prim, primpath_to)
-        new_prim = stage.GetPrimAtPath(primpath_to)
-        target_pos = Gf.Vec3d(tuple(target_points[i]))
-        new_prim.GetAttribute('xformOp:translate').Set(target_pos)
-
-        # Go to the next prim, or reset to 0
-        if cur_idx < num_prims-1: cur_idx +=1
-        else: cur_idx = 0
-
-
+from .core import CurveManager, GeomCreator
 
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
@@ -70,7 +20,6 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
     # this extension is located on filesystem.
         def on_startup(self, ext_id):
             print("[siborg.create.curvedistribute] siborg create curvedistribute startup")
-
 
             #Models
             self._source_prim_model = ui.SimpleStringModel()
@@ -115,50 +64,6 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
                     ui.Spacer()
 
 
-                    def duplicate():
-                        stage = omni.usd.get_context().get_stage()
-
-                        curve_path = '/World/BasisCurves'
-
-                        ## All of these should work (assuming the named prim is there)
-                        ref_prims = ['/World/Cube']
-                        # ref_prims = ['/World/Cube', None]
-                        # ref_prims = ['/World/Cube', '/World/Cone']
-
-                        path_to = f'/Copy'
-                        num_points = self._count
-                        # Default to 3x the number of points to distribute?
-                        num_samples = self._count*3
-
-                        # TODO: make this setting for the resolution to sample the curve defined by user
-                        interpolated_points = interpcurve(stage, curve_path, num_samples)
-                        indices = np.linspace(0, len(interpolated_points) - 1, num_points, dtype=int)
-                        target_points = interpolated_points[indices]
-                        copy_to_points(stage, target_points, ref_prims, path_to)
-
-                    def duplicate():
-
-                        stage = omni.usd.get_context().get_stage()
-                        curve_path = self._source_curve_model.as_string
-                        ref_prims = [self._source_prim_model.as_string]
-
-                        ## All of these should work (assuming the named prim is there)
-                        # ref_prims = ['/World/Cube']
-                        # ref_prims = ['/World/Cube', None]
-                        # ref_prims = ['/World/Cube', '/World/Cone']
-
-                        path_to = f'/Copy'
-
-                        num_points = self._count
-                        # Default to 3x the number of points to distribute? Actually might be handled already by interp
-                        num_samples = self._count
-
-                        # TODO: make this setting for the resolution to sample the curve defined by user
-                        interpolated_points = interpcurve(stage, curve_path, num_samples)
-                        indices = np.linspace(0, len(interpolated_points) - 1, num_points, dtype=int)
-                        target_points = interpolated_points[indices]
-                        copy_to_points(stage, target_points, ref_prims, path_to)
-
                     ui.Label("Set Number of Points")
                     with ui.VStack():
                         distribute_button_style = {"Button":{"background_color": cl.cyan,
@@ -172,7 +77,10 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
                             "Button:hovered":{"background_color": cl("#E5F1FB")}}
                         x = ui.IntField(height=5) 
                         x.model.add_value_changed_fn(lambda m, self=self: setattr(self, '_count', m.get_value_as_int()))
-                        ui.Button("Distribute", clicked_fn=duplicate, style=distribute_button_style)
+                        ui.Button("Distribute", clicked_fn=lambda: GeomCreator.duplicate(self._count,  
+                                                                             self._source_curve_model, 
+                                                                             self._source_prim_model), 
+                                  style=distribute_button_style) 
 
         def on_shutdown(self):
             print("[siborg.create.curvedistribute] siborg create curvedistribute shutdown")
