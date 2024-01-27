e85a8e6dc54acae870841b98796113039c27a406
cadop <cadop@users.noreply.github.com>
2024-01-26 19:07:28 -0500

    add orientation option, add support for bezier and bspline
    cleanup curve distribution
    also some gui updates

--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/core.py
@@ -1,25 +1,36 @@
-from pxr import Usd, UsdGeom, Gf, Sdf
+from pxr import Usd, UsdGeom, Gf, Sdf, Vt
+
 import numpy as np
 from scipy.interpolate import BSpline, CubicSpline
 import omni.usd
 from scipy.interpolate import splprep, splev
 
 
+from . import utils
+
 class CurveManager():
     def __init__(self):
         pass
 
     @classmethod
-    def interpcurve(cls, stage, curve_path, num_points, sampling_resolution):
-        '''Interpolates a bezier curve based on the input points on the usd'''
+    def cubic_bezier(cls, p0, p1, p2, p3, t):
+        '''Calculate position on a cubic Bezier curve given four control points.'''
+        return ((1 - t)**3) * p0 + 3 * ((1 - t)**2) * t * p1 + 3 * (1 - t) * t**2 * p2 + t**3 * p3
 
-        # Get the curve prim and points that define it
-        curveprim = stage.GetPrimAtPath(curve_path)
-        points = curveprim.GetAttribute('points').Get()
-        control_points = np.array(points)
-        
-        # print(control_points)
-        
+    @classmethod
+    def create_bezier(cls, control_points, sampling_resolution):
+        '''Create a continuous cubic Bezier curve from a list of control points.'''
+        curve_points = []
+        t = np.linspace(0, 1, sampling_resolution) # defaulted to 100
+        for i in range(0, len(control_points) - 3, 3):  # Step strides 3, bezier continous shared handles with neighbors
+            p0, p1, p2, p3 = control_points[i], control_points[i + 1], control_points[i + 2], control_points[i + 3]
+            bezier_segment = np.array([CurveManager.cubic_bezier(p0, p1, p2, p3, t_) for t_ in t])
+            curve_points.extend(bezier_segment)
+        return curve_points
+
+    @classmethod
+    def create_bspline(cls, control_points, sampling_resolution):
+        '''Create a continuous cubic Bezier curve from a list of control points.'''
         # Create a BSpline object
         k = 3 # degree of the spline
         t = np.linspace(0, 1, len(control_points) - k + 1, endpoint=True)
@@ -31,12 +42,32 @@ class CurveManager():
         # tnew = np.linspace(0, 1, num_points)
         # interpolated_points = spl(tnew)
         # return interpolated_points
-
+        
         # Calculate the total arc length of the spline
-        if sampling_resolution == 0:
-            sampling_resolution = num_points
         fine_t = np.linspace(0, 1, sampling_resolution)
-        fine_points = spl(fine_t)
+        curve_points = spl(fine_t)
+        
+        return curve_points
+
+    @classmethod
+    def interpcurve(cls, stage, curve_path, num_points, sampling_resolution, curve_type=utils.CURVE.Bspline):
+        '''Interpolates a bezier curve based on the input points on the usd'''
+
+        # Get the curve prim and points that define it
+        curveprim = stage.GetPrimAtPath(curve_path)
+        points = curveprim.GetAttribute('points').Get()
+        control_points = np.array(points)
+        
+        if sampling_resolution == 0: sampling_resolution = num_points
+            
+        if curve_type == utils.CURVE.Bezier:
+            fine_points = CurveManager.create_bezier(control_points, sampling_resolution)
+        elif curve_type == utils.CURVE.Bspline:
+            fine_points = CurveManager.create_bspline(control_points, sampling_resolution)
+        else:
+            print("CURVE TYPE NOT IMPLEMENTED")
+            return
+
         distances = np.sqrt(np.sum(np.diff(fine_points, axis=0)**2, axis=1))
         total_length = np.sum(distances)
 
@@ -48,30 +79,41 @@ class CurveManager():
 
         # Find the points at the target lengths
         spaced_points = [control_points[0]]  # Start with the first control point
+        point_dirs = [fine_points[1]- fine_points[0]] # Store the direction for each point 
         current_length = 0
         j = 1  # Start from the second target length
 
         for i in range(1, len(fine_points)):
             segment_length = distances[i-1]
+            # Create strides to find when the next point should be accepted
             while j < num_points - 1 and current_length + segment_length >= target_lengths[j]:
-                ratio = (target_lengths[j] - current_length) / segment_length
-                point = fine_points[i-1] + ratio * (fine_points[i] - fine_points[i-1])
+                # Assign points
+                point = fine_points[i]                
                 spaced_points.append(point)
+                # Get directions
+                direction = fine_points[i + 1] - point
+                # Normalize the direction vector
+                direction /= np.linalg.norm(direction)
+                point_dirs.append(direction)
+                
                 j += 1
             current_length += segment_length
 
-        spaced_points.append(control_points[-1])  # End with the last control point
-
-        return np.array(spaced_points)
+        # End with the last control point
+        spaced_points.append(control_points[-1])  
+        last_dir = control_points[-1] - control_points[-2]
+        last_dir /= np.linalg.norm(last_dir)
+        point_dirs.append(last_dir)
+        
+        return np.array(spaced_points), np.array(point_dirs)
        
 
     @classmethod
-    def copy_to_points(cls, stage, target_points, ref_prims, path_to, make_instance=False,
+    def copy_to_points(cls, stage, target_points, target_dirs, ref_prims, path_to, _forward_axis, make_instance=False,
                         rand_order=False, use_orient=False, follow_curve=False):
         '''        
         path_to: str, prefix to the prim path. automatically appends Copy
         TODO: rand_order =True, use randomness to determine which prim to place
-        TODO: use_orient=True, rotate object to face direction 
         TODO: follow_curve=True, set rotation axis to match curve (different than orientation when curve is 3d)
         '''
         
@@ -121,6 +163,11 @@ class CurveManager():
                     has_translate_op = any(op.GetOpType() == UsdGeom.XformOp.TypeTranslate for op in xform_ops)
                     if not has_translate_op:
                         xform.AddTranslateOp()
+                        
+                    has_Orient_op = any(op.GetOpType() == UsdGeom.XformOp.TypeOrient for op in xform_ops)
+                    if not has_Orient_op:
+                        xform.AddOrientOp()
+                                                
                     ref_prim= str(new_prim_xform_wrapper.GetPath())
 
                     original_prim = stage.GetPrimAtPath(ref_prim)
@@ -136,6 +183,7 @@ class CurveManager():
         cur_idx = 0
 
 
+        # Place the prims
         for i, target_point in enumerate(target_points):
             ref_prim = prim_set[cur_idx]
 
@@ -148,16 +196,46 @@ class CurveManager():
                 instance_prim = stage.DefinePrim(primpath_to, prim_type)
                 references = instance_prim.GetReferences()
                 references.AddInternalReference(ref_prim.GetPath())
-
-                target_pos = Gf.Vec3d(tuple(target_point))
-                instance_prim.GetAttribute('xformOp:translate').Set(target_pos)
+                # Move prim to desired location
+                cur_prim = instance_prim
+                
             else:
+                # Directly duplicate the prim
                 ref_prim_suffix = str(ref_prim).split('/')[-1]
                 primpath_to = f"{scope_prim.GetPath()}/{ref_prim_suffix}_{i}"
                 omni.usd.duplicate_prim(stage, ref_prim, primpath_to)        
                 new_prim = stage.GetPrimAtPath(primpath_to)
-                target_pos = Gf.Vec3d(tuple(target_point))
-                new_prim.GetAttribute('xformOp:translate').Set(target_pos)
+                cur_prim = new_prim
+                
+            # Move prim to desired location
+            target_pos = Gf.Vec3d(tuple(target_point))
+            cur_prim.GetAttribute('xformOp:translate').Set(target_pos)
+                
+
+            if use_orient:
+                # Make the forward vector face desired direction 
+                target_direction = tuple(target_dirs[i])
+                # Ensure the target direction is a normalized Gf.Vec3f
+                target_direction = Gf.Vec3f(target_direction).GetNormalized()
+                target_direction = np.asarray(target_direction)
+
+                # Forward vector, assuming -Z is forward; change as per your convention
+                forward_vector = np.array(_forward_axis)
+                
+                # Compute the cross product (rotation axis)
+                rotation_axis = np.cross(forward_vector, target_direction)
+                # Compute the dot product and then the angle
+                dot_product = np.dot(forward_vector, target_direction)
+                rotation_angle = np.arccos(np.clip(dot_product, -1.0, 1.0))  # Clipping for numerical stability
+                rot_ax = Vt.Vec3fArray.FromNumpy(rotation_axis)[0]
+
+                # Create a rotation quaternion
+                rotation_quat = Gf.Quatf(rotation_angle, rot_ax).GetNormalized()
+
+                # @TODO fix stupid hack
+                # Apply the rotation
+                try:    cur_prim.GetAttribute('xformOp:orient').Set(Gf.Quatd(rotation_quat))
+                except: cur_prim.GetAttribute('xformOp:orient').Set(Gf.Quatf(rotation_quat))
 
             if cur_idx < num_prims-1: cur_idx +=1
             else: cur_idx = 0
@@ -168,7 +246,8 @@ class GeomCreator():
         pass
     
     @classmethod
-    def duplicate(cls, _count, _sampling_resolution, _source_curve_model, _source_prim_model, _use_instance):
+    def duplicate(cls, _count, _sampling_resolution, _source_curve_model, _source_prim_model, 
+                  _use_instance, _use_orient, _forward_axis, _curve_type):
         '''
         ## All of these should work (assuming the named prim is there)
         # ref_prims = ['/World/Cube']
@@ -182,16 +261,17 @@ class GeomCreator():
         ref_prims = _source_prim_model.as_string.replace(' ','').split(',')
         # _use_instance = _use_instance.as_bool
 
-
         path_to = f'/Copy'
 
-        sampling_resolution = _sampling_resolution
+        sampling_resolution = _sampling_resolution 
         num_points = _count
         # Default to 3x the number of points to distribute? Actually might be handled already by interp
         num_samples = _count
-
+        
         # TODO: make this setting for the resolution to sample the curve defined by user
-        interpolated_points = CurveManager.interpcurve(stage, curve_path, num_samples, sampling_resolution)
+        interpolated_points, target_dirs = CurveManager.interpcurve(stage, curve_path, num_samples, sampling_resolution, _curve_type)
         indices = np.linspace(0, len(interpolated_points) - 1, num_points, dtype=int)
+
         target_points = interpolated_points[indices]
-        CurveManager.copy_to_points(stage, target_points, ref_prims, path_to, make_instance=_use_instance)
+        CurveManager.copy_to_points(stage, target_points, target_dirs, ref_prims, path_to, _forward_axis,
+                                    make_instance=_use_instance, use_orient=_use_orient)
--- exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
+++ exts/siborg.create.curvedistribute/siborg/create/curvedistribute/extension.py
@@ -9,6 +9,9 @@ from . import utils
 
 from .core import CurveManager, GeomCreator
 
+AXIS = ["+X", "+Y", "+Z", "-X", "-Y", "-Z"]  # taken from motion path
+CURVES = ["Bezier", "BSpline"]
+
 # Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() is called.
@@ -27,10 +30,14 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
             self._use_instance_model = ui.SimpleBoolModel()
 
             #Defaults
+            self._count = 0
             self._source_prim_model.as_string = ""
             self._source_curve_model.as_string = ""
+            self._sampling_resolution = 0
             self._use_instance_model = False
-
+            self._use_orient_model = False
+            self._forward_axis = [1,0,0]
+            self._curve_type = utils.CURVE.Bezier
             #Grab Prim in Stage on Selection
             def _get_prim():
                 self._source_prim_model.as_string = ", ".join(utils.get_selection())
@@ -38,9 +45,10 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
             def _get_curve():
                 self._source_curve_model.as_string = ", ".join(utils.get_selection())
 
-            self._window = ui.Window("Distribute Along Curve", width=240, height=320)
+
+            self._window = ui.Window("Distribute Along Curve", width=260, height=360)
             with self._window.frame:
-                with ui.VStack(height=10, width=220, spacing=10):
+                with ui.VStack(height=10, width=240, spacing=10):
                     select_button_style ={"Button":{"background_color": cl.cyan,
                                                 "border_color": cl.white,
                                                 "border_width": 2.0,
@@ -51,14 +59,14 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
                                                 "Button.Label":{"color": cl.black},
                                                 "Button:hovered":{"background_color": cl("#E5F1FB")}}
                     ui.Spacer()
-                    ui.Label("Select Curve From Stage")
+                    ui.Label("Select Curve From Stage", tooltip="Select a BasisCurves type")
                     with ui.HStack():
                         ui.StringField(height=2, model=self._source_curve_model)
                         ui.Button("S", width=20, height=20, style=select_button_style, clicked_fn=_get_curve)
 
                     ui.Spacer()
 
-                    label = ui.Label("Get Prim From Stage", width=65)
+                    ui.Label("Get Prims From Stage", width=65, tooltip="Select multiple prims to distribute in sequence")
                     with ui.HStack():
                         ui.StringField(height=2, model=self._source_prim_model)
                         ui.Button("S", width=20, height=20, style=select_button_style, clicked_fn=_get_prim)
@@ -66,15 +74,14 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
                     ui.Spacer()
 
 
-
                     with ui.HStack():
-                        ui.Label("Copies")
+                        ui.Label("Copies", tooltip="Number of copies to distribute. Endpoints are included in the count.")
                         x = ui.IntField(height=5)
-                        x.model.set_value(0) 
                         x.model.add_value_changed_fn(lambda m, self=self: setattr(self, '_count', m.get_value_as_int()))
+                        x.model.set_value(3) 
                         
                         
-                        ui.Label(" Subsamples")
+                        ui.Label("     Subsamples", tooltip="A sampling parameter, don't touch if you don't understand")
                         x = ui.IntField(height=5) 
                         x.model.add_value_changed_fn(lambda m, self=self: setattr(self, '_sampling_resolution', m.get_value_as_int()))
                         x.model.set_value(1000) 
@@ -96,17 +103,49 @@ class SiborgCreateCurvedistributeExtension(omni.ext.IExt):
                                                                                          self._sampling_resolution, 
                                                                                          self._source_curve_model, 
                                                                                          self._source_prim_model, 
-                                                                                         self._use_instance_model), 
+                                                                                         self._use_instance_model,
+                                                                                         self._use_orient_model,
+                                                                                         self._forward_axis,
+                                                                                         self._curve_type), 
                                         style=distribute_button_style) 
 
                     with ui.HStack():
                         # ui.StringField(height=2, model=self._source_prim_model)
                         # ui.Button("S", width=20, height=20, style=select_button_style, clicked_fn=_get_prim)
-                        ui.Label(" Use instances ", width=65)
-                                 
+                        ui.Label(" Use instances ", width=65, tooltip="Select to use instances when copying a prim")
                         instancer = ui.CheckBox(width=30)
                         instancer.model.add_value_changed_fn(lambda m : setattr(self, '_use_instance_model', m.get_value_as_bool()))
                         instancer.model.set_value(False)
+                        
+                        ui.Label(" Follow Orientation ", width=65)
+                        instancer = ui.CheckBox(width=30)
+                        instancer.model.add_value_changed_fn(lambda m : setattr(self, '_use_orient_model', m.get_value_as_bool()))
+                        instancer.model.set_value(False)
 
+                    with ui.HStack():
+                        ui.Label("Spline Type", 
+                                 name="label", 
+                                 width=160, 
+                                 tooltip="Type of curve to use for interpolating control points")
+                        ui.Spacer(width=13)
+                        widget = ui.ComboBox(0, *CURVES).model
+                        widget.add_item_changed_fn(lambda m, i: setattr(self, '_curve_type',
+                                                                        m.get_item_value_model().get_value_as_int()
+                                                                        )
+                                                   )
+
+                    with ui.HStack():
+                        ui.Label("Forward Axis", 
+                                 name="label", 
+                                 width=160, 
+                                 tooltip="Forward axis of target Object")
+                        ui.Spacer(width=13)
+                        widget = ui.ComboBox(0, *AXIS).model
+                        widget.add_item_changed_fn(lambda m, i: setattr(self, '_forward_axis',
+                                                                        utils.index_to_axis(m.get_item_value_model().get_value_as_int())
+                                                                        )
+                                                   )
+        
         def on_shutdown(self):
             print("[siborg.create.curvedistribute] siborg create curvedistribute shutdown")
+
